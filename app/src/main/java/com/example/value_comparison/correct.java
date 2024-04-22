e98b0b27969c7b99c6cd6e3b0c24fcc941b64845
megure-666 <megure666@gmail.com>
2024-04-22 13:46:03 +0900

    the field of correct change

--- app/src/main/java/com/example/value_comparison/correct.java
+++ app/src/main/java/com/example/value_comparison/correct.java
@@ -19,4 +19,9 @@ public class correct extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void onButton_quiz(View view) {
+        Intent intent = new Intent(this, quiz.class);
+        startActivity(intent);
+    }
 }
