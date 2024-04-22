7215107d381148d347bea062aecf416fb39dd177
megure-666 <megure666@gmail.com>
2024-04-22 13:48:52 +0900

    the field of incorrect change

--- app/src/main/java/com/example/value_comparison/incorrect.java
+++ app/src/main/java/com/example/value_comparison/incorrect.java
@@ -18,4 +18,9 @@ public class incorrect extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void onButton_quiz(View view) {
+        Intent intent = new Intent(this, quiz.class);
+        startActivity(intent);
+    }
 }
